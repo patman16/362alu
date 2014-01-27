0a134c93ab936e4c3c97289b78809ae2f9662185
Patrick Joseph Hayes <pjh693@nightwing.ece.northwestern.edu>
2014-01-27 13:41:12 -0600

    finished greater than

--- sets.v
+++ sets.v
@@ -22,7 +22,6 @@ module equal(a,b,eq);
 		assign eq = &x;
 endmodule
 
-/*
 module aNotb(a,b, test);
         input a, b;
         output test;
@@ -31,24 +30,30 @@ module aNotb(a,b, test);
 endmodule
 
 module greaterThan(a,b,eq);
-        parameter N = 4;
+        parameter N = 32;
         input signed [N-1:0]  a, b;
         output eq;
         wire [N-1:0] x;
-        wire  [N-1:0] test;
+        wire  [N-1:0] anotbvector;
+	wire [N-2:0] ineqvector;
+	wire mageq;
 
         generate
         genvar i;
         for(i=0; i<(N); i = i+1)
                 begin : uF
                 constant GENERATE(a[i], b[i], x[i]);
-                aNotb AB(a[i],b[i],test[i]);
+                aNotb AB(a[i],b[i],anotbvector[i]);
         end
+	for (i=N-2; i >= 0; i = i-1) begin: ineq
+		assign ineqvector[i] = &x[N-1:((N-1)-(N-2-i))] & anotbvector[i]; 	
+	end
         endgenerate
 
-        assign eq = test[3] | x[3]&test[2] | x[3]&x[2]&test[1] | x[3]&x[2]&x[1]&test[0];
+	assign mageq = anotbvector[N-1] | |ineqvector;
+	assign eq = ((a[N-1] == 1'b0 && b[N-1] == 1'b1) || (a[N-1] == 1'b1 && b[N-1] == 1'b0)) ? ~mageq : mageq;
 endmodule
-
+/*
 module greaterOrEqual(a,b,eq);
 	parameter N = 4;
 	input [N-1:0] a, b;
