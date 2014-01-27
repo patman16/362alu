3d1cb77fb017d9a25fc95f9f53588cc2e8956f96
Patrick Joseph Hayes <pjh693@nightwing.ece.northwestern.edu>
2014-01-27 14:25:28 -0600

    finished comparator components

--- sets.v
+++ sets.v
@@ -53,9 +53,9 @@ module greaterThan(a,b,eq);
 	assign mageq = anotbvector[N-1] | |ineqvector;
 	assign eq = ((a[N-1] == 1'b0 && b[N-1] == 1'b1) || (a[N-1] == 1'b1 && b[N-1] == 1'b0)) ? ~mageq : mageq;
 endmodule
-/*
+
 module greaterOrEqual(a,b,eq);
-	parameter N = 4;
+	parameter N = 32;
 	input [N-1:0] a, b;
 	output eq;
 	wire [N-1:0] x;
@@ -68,44 +68,46 @@ module greaterOrEqual(a,b,eq);
 
 endmodule
 
-module notAB(a,b, test);
+module Notab(a,b, test);
         input a, b;
         output test;
 
         assign test = ~a & b;
 endmodule
 
-
 module lessThan(a,b,eq);
-	parameter N = 4;
-	input [N-1:0] a, b;
-	output eq;
-	wire [N-1:0] x;
-	wire [N-1:0] test;
+	parameter N = 32;
+    input signed [N-1:0]  a, b;
+    output eq;
+    wire [N-1:0] x;
+    wire  [N-1:0] notabvector;
+	wire [N-2:0] ineqvector;
+	wire mageq;
 
-	generate
-	genvar i;
-	for(i=0; i<N; i = i+1)
-		begin : uF
-		constant GENERATE(a[i], b[i], x[i]);
-		notAB NOTAB(a[i], b[i], test[i]);
+        generate
+        genvar i;
+        for(i=0; i<(N); i = i+1)
+                begin : uF
+                constant GENERATE(a[i], b[i], x[i]);
+                Notab AB(a[i],b[i],notabvector[i]);
+        end
+	for (i=N-2; i >= 0; i = i-1) begin: ineq
+		assign ineqvector[i] = &x[N-1:((N-1)-(N-2-i))] & notabvector[i]; 	
 	end
-	endgenerate
+        endgenerate
 
-	assign eq = test[3] | x[3]&test[2] | x[3]&x[2]&test[1] | x[3]&x[2]&x[1]&test[0];
+	assign mageq = notabvector[N-1] | |ineqvector;
+	assign eq = ((a[N-1] == 1'b0 && b[N-1] == 1'b1) || (a[N-1] == 1'b1 && b[N-1] == 1'b0)) ? ~mageq : mageq;
 endmodule
 
 module lessOrEqual(a,b,eq);
-        parameter N = 4;
+        parameter N = 32;
         input [N-1:0] a, b;
         output eq;
-        wire [N-1:0] x;
         wire  test1, test2;
 
       	lessThan LESS(a,b,test1);
   	equal EQ(a,b,test2);
 
         assign eq = test1 | test2;
-
 endmodule
-*/
---
+++ tests/lessThan_test.v
@@ -0,0 +1,22 @@
+module testbench;
+
+        reg signed [31:0] A;
+        reg signed [31:0] B;
+        wire EQUIVALENT;
+
+        lessThan LESS(.a(A), .b(B), .eq(EQUIVALENT));
+
+        initial begin
+        $monitor("A = %d B = %d LESSTHAN = %b", A, B, EQUIVALENT);
+        #0 A = 1; B = 2;
+		#1 A = 2; B = 1;
+		#1 A = -2; B = -1;
+		#1 A = -1; B = -2;
+		#1 A = 0; B = 0;
+		#1 A = -2; B = 1;
+		#1 A = 2; B = -1;
+		#1 A = 1; B = -2;
+		#1 A = -1; B = 2;
+        end
+endmodule
+
